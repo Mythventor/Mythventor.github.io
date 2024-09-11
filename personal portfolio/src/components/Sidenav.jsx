d4d95b3091a69b8e28e63175dc6401891567ab5e
Mythventor <151417971+Mythventor@users.noreply.github.com>
2024-09-11 03:07:30 -0500

    Personal Portfolio

--- personal portfolio/src/components/Sidenav.jsx
+++ personal portfolio/src/components/Sidenav.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
+
 
 const Sidenav = () => {
+    const [nav, setNav] = useState(false);
+    const handleNav = () =>{
+        setNav(!nav);
+        console.log('tap');
+    };
+
+
   return (
-    <div>Sidenav</div>
-  )
-}
+    <div>
+        <AiOutlineMenu onClick= {handleNav} className = 'absolute top-4 right-4 z-[99] md:hidden'/>
+        {
+            nav ? (
+                <div> 
+                    <a href=""></a>
+                </div>
+            )
+            :(
+                <div></div>
+            )
+        }
+
+    </div>
+
+  );
+};
 
 export default Sidenav
