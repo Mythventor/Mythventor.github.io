f2fecdc3aec6c15f86796f2e47ebb7e8c720cec1
Mythventor <151417971+Mythventor@users.noreply.github.com>
2024-09-11 13:58:56 -0500

    Update Sidenav.jsx

--- personal portfolio/src/components/Sidenav.jsx
+++ personal portfolio/src/components/Sidenav.jsx
@@ -12,37 +12,37 @@ const Sidenav = () => {
 
   return (
     <div>
-        {/* Create a menu from the top right*/}
+        {/* Create a menu from the top right (phone mode)*/}
         <AiOutlineMenu onClick= {handleNav} className = 'absolute top-4 right-4 z-[99] md:hidden'/>
         {
             nav ? (
                 <div className='fixed w-full h-screen bg-white/90 flex flex-col  justify-center items-center z-20'> 
-                    {/* Create a menu option for Home page */}
+                    {/* Create a menu option for Home page (phone mode)*/}
                     <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineHome size={20}/>
                         <span className='pl-4'>Home</span>
                     </a>
 
 
-                    {/* Create a menu option for Work page */}
+                    {/* Create a menu option for Work page (phone mode)*/}
                     <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <GrProjects size={20}/>
                         <span className='pl-4'>Work</span>
                     </a>
 
-                    {/* Create a menu option for Project page */}
+                    {/* Create a menu option for Project page (phone mode)*/}
                     <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineProject size={20}/>
                         <span className='pl-4'>Project</span>
                     </a>
                     
-                    {/* Create a menu option for Resume page */}
+                    {/* Create a menu option for Resume page (phone mode)*/}
                     <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <BsPerson size={20}/>
                         <span className='pl-4'>Resume</span>
                     </a>
 
-                    {/* Create a menu option for Contact page */}
+                    {/* Create a menu option for Contact page (phone mode)*/}
                     <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineMail size={20}/>
                         <span className='pl-4'>Contact</span>
@@ -50,13 +50,22 @@ const Sidenav = () => {
                 </div>
             )
             :(
-                <div></div>
+                ''
             )
         }
+
+        <div className='md:block hidden fixed top-[25%] z-10'>
+            <div className='flex flex-col'>
+                <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlineHome size={20}/>
+
+                </a>
+            </div>
+        </div>
+
 
     </div>
 
   );
 };
-
 export default Sidenav
