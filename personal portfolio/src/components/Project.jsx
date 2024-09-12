ba182c2a6fe36ff2d55847d7be4e4c4575af20e9
Mythventor <151417971+Mythventor@users.noreply.github.com>
2024-09-11 21:18:39 -0500

    personal portfolio

--- personal portfolio/src/components/Project.jsx
+++ personal portfolio/src/components/Project.jsx
@@ -7,9 +7,10 @@ const Project = () => {
     <div id='project' className='max-w-[1048px] m-auto md:pl-20 p-4 py-16'>
 
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Project</h1>
-        <p className='text-center py-8'>
-            anjskdnkajsndjkasndjkasndjknasdjknnsjdknaskjdnasjkdnjkasndjkasndjkansdjknasjkdnajskndjkasndjksad
+        <p className='text-center py-8 text-lg font-semibold text-gray-700'>
+            Explore the complete list of my projects below, showcasing my experience and skills in software development, web design, and engineering solutions.
         </p>
+
 
         <div className='grid sm:grid-cols-2 gap-12'>
             <ProjectItem img={SnakeGameProjectImg} title='Snake Game'></ProjectItem>
