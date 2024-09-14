4b5cd6f728f9c3e84ba957b2b1e1320b0a335ba7
Mythventor <151417971+Mythventor@users.noreply.github.com>
2024-09-13 20:23:21 -0500

    personal portfolio

--- personal portfolio/src/components/Project.jsx
+++ personal portfolio/src/components/Project.jsx
@@ -14,12 +14,12 @@ const Project = () => {
             Explore the complete list of my projects below, showcasing my experience and skills in software development, web design, and engineering solutions.
         </p>
 
-
+        {/* Project Title, Programming Language, and Link */}
         <div className='grid sm:grid-cols-2 gap-12'>
             <ProjectItem img={SnakeGameProjectImg} title='Snake Game' ProgrammingLanguage='DrRacket' ProjectLink='https://github.com/Mythventor/SnakeGame.git'></ProjectItem>
-            <ProjectItem img={SpeechRecognitionProjectImg} title='Speech Recognition App' ProgrammingLanguage='Python' ProjecyLink='https://github.com/Mythventor/Voice-To-Text.git'> </ProjectItem>
-            <ProjectItem img={PersonalWebProject} title='Personal Portfolio' ProgrammingLanguage='React+ViteJS+TailwindCSS' ProjecyLink='https://github.com/Mythventor/Mythventor.github.io.git'></ProjectItem>
-            <ProjectItem img={MatherChessProjectImg} title='Mather HS Chess Website' ProgrammingLanguage='React+CSS+JS' ProjecyLink='https://mythventor.github.io/mather-chess/'></ProjectItem>
+            <ProjectItem img={SpeechRecognitionProjectImg} title='Speech Recognition App' ProgrammingLanguage='Python' ProjectLink='https://github.com/Mythventor/Voice-To-Text.git'> </ProjectItem>
+            <ProjectItem img={PersonalWebProject} title='Personal Portfolio' ProgrammingLanguage='React+ViteJS+TailwindCSS' ProjectLink='https://github.com/Mythventor/Mythventor.github.io.git'></ProjectItem>
+            <ProjectItem img={MatherChessProjectImg} title='Mather HS Chess Website' ProgrammingLanguage='React+CSS+JS' ProjectLink='https://mythventor.github.io/mather-chess/'></ProjectItem>
 
         </div>
 
